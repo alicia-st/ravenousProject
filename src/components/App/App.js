0b3644167c21fa985e1a3f27376267d385c71f25
Alicia <76852139+alicia-st@users.noreply.github.com>
2023-10-31 13:20:09 +0200

    Introduced all my components in the parent component (App)

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,13 @@
 
 import BusinessList from '../BusinessList/BusinessList.js';
 import SearchBar from '../SearchBar/SearchBar.js';
+import styles from './App.module.css'
 function App() {
-    return (<>
+    return (<div className={styles.App}>
+        <h1>ravenous</h1>
         <SearchBar />
         <BusinessList />
-    </>);
+    </div>);
 };
 
 export default App;
